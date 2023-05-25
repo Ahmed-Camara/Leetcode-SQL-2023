a493ae7592e29541f609527a19a6171f574b6d4d
Ahmed-Camara <camarahmed123@gmail.com>
2023-05-25 19:13:05 +0100

    1070. Product Sales Analysis III

--- March/ProductSalesAnalysis_III.sql
+++ March/ProductSalesAnalysis_III.sql
@@ -64,3 +64,9 @@ Output:
 | 200        | 2011       | 15       | 9000  |
 +------------+------------+----------+-------+
 */
+
+SELECT product_id, year as first_year, quantity, price
+  FROM sales
+ WHERE (product_id, year) in (SELECT product_id, min(year)
+                                FROM sales
+                            GROUP BY product_id);
